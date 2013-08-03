0546df2b8a530ca938b003f8ad3e53829fe1a001
abhishekparihar <abhishek@weboniselab.com>
2013-08-03 13:36:53 +0530

    added Screen for login

--- src/com/weboapps/badmintonbuddy/LoginActivity.java
+++ src/com/weboapps/badmintonbuddy/LoginActivity.java
@@ -1,19 +1,39 @@
 package com.weboapps.badmintonbuddy;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends Activity {
 
+	
+	EditText editTextUser, editTextPassword;
+	Button buttonLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.login);
+        
+        initializeWidgets();
     }
 
 
-    @Override
+    private void initializeWidgets() {
+    	editTextUser = (EditText)findViewById(R.id.editTextUser);
+    	editTextPassword = (EditText)findViewById(R.id.editTextPassword);
+    	buttonLogin = (Button)findViewById(R.id.buttonLogin);
+	}
+    
+    
+    public void onLoginClick(View v) {
+    	
+    }
+
+
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.badminton_buddy_native, menu);
