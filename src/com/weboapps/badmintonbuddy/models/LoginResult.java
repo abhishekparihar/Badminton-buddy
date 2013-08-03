089366121f7c8712b988fac25db380f4c4fb2bec
abhishekparihar <abhishek@weboniselab.com>
2013-08-03 16:42:34 +0530

    added sliding menu

--- src/com/weboapps/badmintonbuddy/models/LoginResult.java
+++
@@ -1,9 +0,0 @@
-package com.weboapps.badmintonbuddy.models;
-
-
-public class LoginResult  {
-    private Boolean login;
-    private String api_key;
-    private String message;
-   
-}
--- src/com/weboapps/badmintonbuddy/services/WebServiceIface.java
+++
@@ -1,6 +0,0 @@
-package com.weboapps.badmintonbuddy.services;
-
-
-public interface WebServiceIface {
-    public static final String BASE_URL = "http://";
-}
